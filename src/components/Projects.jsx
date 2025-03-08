1f937cd05fc3c0aee6cc570533d9fbbf6e72aca5
Natallia-Karatava <nkaratova@gmail.com>
2025-03-08 14:08:03 +0100

    style img

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,15 +4,17 @@ import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
 const Carousel = () => {
   const projects = [
     {
-      name: "Simple page - just HTML and CSS",
-      image: "src/images/projects-img/simple-page-html-css.png",
-      url: "https://github.com/Natallia-Karatava/landing-page-projeckt?tab=readme-ov-file",
-    },
-    {
       name: "Website for the artist",
-      image: "src/images/projects-img/website-for-the-artist-min.png",
+      image: "src/images/projects-img/website-for-the-artist.png",
       url: "https://helena-klaus.de/galerie/",
     },
+
+    {
+      name: "Simple page - just HTML and CSS",
+      image: "src/images/projects-img/simple-page-only-html-css.png",
+      url: "https://github.com/Natallia-Karatava/landing-page-projeckt?tab=readme-ov-file",
+    },
+
     {
       name: "Guess the color game!",
       image: "src/images/projects-img/game-guess-the-color.png",
@@ -21,7 +23,7 @@ const Carousel = () => {
 
     {
       name: "Website for an IT company - design",
-      image: "src/images/projects-img/website-for-an-it-company.png",
+      image: "src/images/projects-img/website-for-it-company.png",
       url: "https://visutechsystem.by/en/",
     },
     {
@@ -31,8 +33,13 @@ const Carousel = () => {
     },
     {
       name: "App for event search - design",
-      image: "src/images/projects-img/App-LAZY.png",
+      image: "src/images/projects-img/app-lazy.png",
       url: "https://www.behance.net/gallery/134278605/koncept-mobilnogo-prilozhenija-dlja-poiska-sobytij",
+    },
+    {
+      name: "Coffee shop app - design",
+      image: "src/images/projects-img/app-coffe.png",
+      url: "https://www.behance.net/gallery/133828119/koncept-mobilnogo-prilozhenija-dlja-kofejni",
     },
   ];
 
@@ -78,65 +85,57 @@ const Carousel = () => {
       <div className="relative w-full overflow-visible">
         <h2 className="text-4xl font-semibold text-center mb-8">My Projects</h2>
         <div className="relative max-w-6xl mx-auto flex items-center justify-center overflow-visible">
-          {/* Кнопка влево */}
+          {/* Link button */}
           <button
             onClick={handlePrev}
-            className="absolute left-0 z-10 text-white bg-gray-800 hover:bg-red-600 hover:text-gray-800 p-3 rounded-full m-10"
+            className="hidden md:block absolute left-0 z-10 text-white bg-gray-800 hover:bg-red-600 hover:text-gray-800 p-3 rounded-full m-10"
           >
             <FaArrowLeft size={24} />
           </button>
 
-          {/* Контейнер с карточками */}
+          {/* Card */}
           <div className="flex w-full justify-around items-center gap-2 sm:gap-4 md:gap-6">
             {visibleProjects.map((project, index) => (
               <div
                 key={index}
                 className={`flex-shrink-0 transition-transform duration-500 ease-out ${
-                  index === 1 ? "scale-100 opacity-100" : "scale-90 opacity-50 "
+                  index === 1 ? "scale-100 opacity-100" : "scale-85 opacity-50 "
                 } w-11/12 sm:w-9/12 md:w-1/3`}
-                style={{
-                  transform:
-                    index === 0
-                      ? "translateX(0%)"
-                      : index === 2
-                      ? "translateX(0%)"
-                      : "translateX(0)",
-                }}
               >
-                <div className="rounded-lg overflow-hidden shadow-lg">
-                  {/* Верхняя часть карточки — фон с изображением */}
-                  <div
-                    className="min-h-[15rem] bg-cover bg-center transition-all duration-500"
-                    style={{
-                      backgroundImage: `url(${project.image})`,
-                      backgroundSize: "cover", // Фиксируем размер
-                      backgroundPosition: "center", // Центрируем изображение
-                      filter: index === 1 ? "none" : "grayscale(100%)",
-                    }}
-                  ></div>
-                  {/* Нижняя белая плашка */}
-                  <div className="bg-white p-4">
-                    <h3 className="text-lg font-semibold text-gray-800 ml-2">
-                      {project.name}
-                    </h3>
-                    <a
-                      href={project.url}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className="mt-2 inline-block text-gray-800  px-2 hover:text-red-600 transition duration-300"
-                    >
-                      Go to view
-                    </a>
+                <a href={project.url} target="_blank" rel="noopener noreferrer">
+                  <div className="rounded-lg overflow-hidden shadow-lg">
+                    {/*Top card - img */}
+                    <div
+                      className="w-full max-w-[400px] aspect-[3/2] bg-cover bg-center transition-all duration-500"
+                      style={{
+                        backgroundImage: `url(${project.image})`,
+                        filter: index === 1 ? "none" : "grayscale(100%)",
+                      }}
+                    ></div>
+                    {/* Botton card*/}
+                    <div className="bg-white p-4">
+                      <h3 className="text-lg font-semibold text-gray-800 ml-2">
+                        {project.name}
+                      </h3>
+                      <a
+                        href={project.url}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="mt-2 inline-block text-gray-800  px-2 hover:text-red-600 transition duration-300"
+                      >
+                        Go to view
+                      </a>
+                    </div>
                   </div>
-                </div>
+                </a>
               </div>
             ))}
           </div>
 
-          {/* Кнопка вправо */}
+          {/* Right button */}
           <button
             onClick={handleNext}
-            className="absolute right-0 z-10 text-white bg-gray-800 p-3 hover:bg-red-600 hover:text-gray-800 rounded-full m-10"
+            className=" hidden md:block absolute right-0 z-10 text-white bg-gray-800 p-3 hover:bg-red-600 hover:text-gray-800 rounded-full m-10"
           >
             <FaArrowRight size={24} />
           </button>
